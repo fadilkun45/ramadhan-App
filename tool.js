3829e8717bc84665d920d9a2a7d926008d81166a
fadilkun45 <indocrafter76@gmail.com>
2021-03-27 00:24:30 +0700

    update typewriter

--- tool.js
+++ tool.js
@@ -3,6 +3,8 @@ const RenderTanggal = document.querySelector(".tgl-adzan")
 const month = [
     "","Januari","Febuari","Maret","April","Mei","Juni","Juli","Agustus", "September","Oktober","November","Desember",
 ];
+const Typewriters = document.querySelector("#typewriter")
+
 
 fetch("https://api.pray.zone/v2/times/today.json?city=jakarta")
 .then((res) => res.json())
@@ -43,6 +45,46 @@ fetch("https://api.pray.zone/v2/times/today.json?city=jakarta")
     console.log(time)
 })
 
+
+// typewriter
+const textArray = ["Masih dalam tahap prototype", "mohon bersabar","semoga ketemu API yang pas buat tool ini"];
+const typingDelay = 50;
+const erasingDelay = 100;
+const newTextDelay = 500; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    Typewriters.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    
+    Typewriters.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+
+
 // Mengatur waktu akhir perhitungan mundur
  Hitungmundur = new Date("April 13, 2021 00:00:00").getTime();
 
@@ -51,18 +93,15 @@ let Countdown = setInterval(function() {
   var now = new Date().getTime();
     
   var distance = Hitungmundur - now;
-    
-  // Perhitungan waktu untuk hari, jam, menit dan detik
+
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Keluarkan hasil dalam elemen dengan id = "demo"
   document.querySelector("#time").innerHTML = days + " hari " + hours + " jam "
   + minutes + " menit " + seconds + " detik";
     
-  // Jika hitungan mundur selesai, tulis beberapa teks 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "Alhamdulilah Kita Sudah masuk bulan suci Ramadhan";
